2a43f9105e1fb47a68dcd83262d3f521101e4828
Alexey <Fyodorov.aleksej@gmail.com>
2017-10-03 10:07:09 +0300

    update read synchronization

---
+++ lpm_dff2_inst.vhd
@@ -0,0 +1,9 @@
+lpm_dff2_inst : lpm_dff2 PORT MAP (
+		aclr	 => aclr_sig,
+		aload	 => aload_sig,
+		aset	 => aset_sig,
+		clock	 => clock_sig,
+		data	 => data_sig,
+		enable	 => enable_sig,
+		q	 => q_sig
+	);
